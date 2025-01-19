a795605d3af413a075e76d39837352f0b98ed7b5
Altermanns <isaac.277.trujillo@gmail.com>
2025-01-18 19:16:55 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoProgreso3/Services/DatabaseService.cs
@@ -0,0 +1,39 @@
+﻿using ProyectoProgreso3.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoProgreso3.Services
+{
+    public class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public DatabaseService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Usuario>().Wait();
+        }
+
+        public Task<List<Usuario>> GetVendedoresAsync()
+        {
+            return _database.Table<Usuario>().ToListAsync();
+        }
+
+        public Task<int> SaveVendedorAsync(Usuario vendedor)
+        {
+            if (vendedor.IdUsuario != 0)
+                return _database.UpdateAsync(vendedor);
+            else
+                return _database.InsertAsync(vendedor);
+        }
+
+        public Task<int> DeleteVendedorAsync(Usuario vendedor)
+        {
+            return _database.DeleteAsync(vendedor);
+        }
+    }
+}
---
+++ ProyectoProgreso3/ViewsModels/UsuarioViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Windows.Input;
+
+namespace ProyectoProgreso3.ViewsModels
+{
+    public class UsuarioViewModel
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public ICommand RegistrarVendedorCommand { get; set; }
+
+        public UsuarioViewModel()
+        {
+            Nombre = string.Empty;
+            Correo = string.Empty;
+            RegistrarVendedorCommand = new Command(RegistrarUsuario);
+        }
+
+        private void RegistrarUsuario()
+        {
+            System.Diagnostics.Debug.WriteLine($"Vendedor registrado: {Nombre}, {Correo}");
+        }
+    }
+
+}
